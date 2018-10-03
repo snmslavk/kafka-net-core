d1b3537264cb936d4d790da57094c7685244ad8c
Viacheslav Avsenev <v.avsenev@kendaxa.com>
2018-10-03 12:04:30 +0200

    init

---
+++ kafka-net-core/Common/AsyncCollection.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KafkaNet.Common
+{
+    public class AsyncCollection<T>
+    {
+        private readonly object _lock = new object();
+        private readonly AsyncManualResetEvent _dataAvailableEvent = new AsyncManualResetEvent();
+        private readonly ConcurrentQueue<T> _queue = new ConcurrentQueue<T>();
+        private long _dataInBufferCount = 0;
+
+        public int Count
+        {
+            get { return _queue.Count + (int)Interlocked.Read(ref _dataInBufferCount); }
+        }
+
+        public bool IsCompleted { get; private set; }
+
+        public void CompleteAdding()
+        {
+            IsCompleted = true;
+        }
+
+        public Task OnHasDataAvailable(CancellationToken token)
+        {
+            return _dataAvailableEvent.WaitAsync().WithCancellation(token);
+        }
+
+        public void Add(T data)
+        {
+            if (IsCompleted)
+            {
+                throw new ObjectDisposedException("AsyncCollection has been marked as complete.  No new documents can be added.");
+            }
+
+            _queue.Enqueue(data);
+
+            TriggerDataAvailability();
+        }
+
+        public void AddRange(IEnumerable<T> data)
+        {
+            if (IsCompleted)
+            {
+                throw new ObjectDisposedException("AsyncCollection has been marked as complete.  No new documents can be added.");
+            }
+
+            foreach (var item in data)
+            {
+                _queue.Enqueue(item);
+            }
+
+            TriggerDataAvailability();
+        }
+
+        public T Pop()
+        {
+            return TryTake(out T data) ? data : default(T);
+        }
+
+        public async Task<List<T>> TakeAsync(int count, TimeSpan timeout, CancellationToken token)
+        {
+            var batch = new List<T>(count);
+            var timeoutTask = Task.Delay(timeout, token);
+
+            try
+            {
+                do
+                {
+                    while (TryTake(out T data))
+                    {
+                        batch.Add(data);
+                        Interlocked.Increment(ref _dataInBufferCount);
+                        if (--count <= 0 || timeoutTask.IsCompleted) return batch;
+                    }
+                } while (await Task.WhenAny(_dataAvailableEvent.WaitAsync(), timeoutTask) != timeoutTask);
+
+                return batch;
+            }
+            catch
+            {
+                return batch;
+            }
+            finally
+            {
+                Interlocked.Add(ref _dataInBufferCount, -1 * batch.Count);
+            }
+        }
+
+        public void DrainAndApply(Action<T> appliedFunc)
+        {
+            while (_queue.TryDequeue(out T data))
+            {
+                appliedFunc(data);
+            }
+
+            TriggerDataAvailability();
+        }
+
+        public IEnumerable<T> Drain()
+        {
+            while (_queue.TryDequeue(out T data))
+            {
+                yield return data;
+            }
+
+            TriggerDataAvailability();
+        }
+
+        public bool TryTake(out T data)
+        {
+            try
+            {
+                return _queue.TryDequeue(out data);
+            }
+            finally
+            {
+                if (_queue.IsEmpty) TriggerDataAvailability();
+            }
+        }
+
+        private void TriggerDataAvailability()
+        {
+            if (_queue.IsEmpty && _dataAvailableEvent.IsOpen)
+            {
+                lock (_lock)
+                {
+                    if (_queue.IsEmpty && _dataAvailableEvent.IsOpen)
+                    {
+                        _dataAvailableEvent.Close();
+                    }
+                }
+            }
+
+            if (!_queue.IsEmpty && !_dataAvailableEvent.IsOpen)
+            {
+                lock (_lock)
+                {
+                    if (!_queue.IsEmpty && !_dataAvailableEvent.IsOpen)
+                    {
+                        _dataAvailableEvent.Open();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ kafka-net-core/Common/ConcurrentCircularBuffer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace KafkaNet.Common
+{
+    public class ConcurrentCircularBuffer<T> : IEnumerable<T>
+    {
+        private readonly int _maxSize;
+        private long _count;
+        private int _head = -1;
+        readonly T[] _values;
+
+        public ConcurrentCircularBuffer(int max)
+        {
+            _maxSize = max;
+            _values = new T[_maxSize];
+        }
+
+        public int MaxSize { get { return _maxSize; } }
+
+        public long Count
+        {
+            get
+            {
+                return Interlocked.Read(ref _count);
+            }
+        }
+
+        public ConcurrentCircularBuffer<T> Enqueue(T obj)
+        {
+            var head = Interlocked.Increment(ref _head);
+
+            if (head > _maxSize - 1)
+            {
+                //this should exchange to correct index even if interlocked called twice from different threads
+                Interlocked.Exchange(ref _head, head - _maxSize);
+                head = head - _maxSize;
+            }
+
+            _values[head] = obj;
+
+            if (_count != _maxSize) //once we hit max size we dont need to track count.
+                Interlocked.Exchange(ref _count, Math.Min(Interlocked.Increment(ref _count), _maxSize));
+
+            return this;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return _values[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
